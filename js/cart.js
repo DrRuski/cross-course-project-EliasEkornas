b652978ccd8a03a61094837c14155f6bfc014093
DrRuski <ilia_e@hotmail.com>
2022-10-07 09:59:30 +0200

    commit

--- js/cart.js
+++ js/cart.js
@@ -2,10 +2,7 @@ import { gameProducts } from "./array/arr.js";
 
 let arrCount = 0;
 
-
-
 for (let index = 0; index < gameProducts.length; index++) {
-    const element = gameProducts[index]
-    console.log(element)
-    
+  const element = gameProducts[index];
+  console.log(element);
 }
